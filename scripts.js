14b082f49757854b32ff3582aaf159133bd05bdc
RuhieJabbal2510 <ruhie25jabbal@gmail.com>
2023-08-08 13:39:10 +1000

    db get post

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const cardList = [{
+/*const cardList = [{
     title: 'Beluga ',
     path: 'images/beluga.jpeg',
     subTitle: 'About Beluga',
@@ -15,14 +15,14 @@ const cardList = [{
     path: 'images/orca.jpg',
     subTitle: 'About Orca',
     description: ' The orca (killer whale) is a toothed whale and is the largest member of the Dolphin family. These large marine mammals are easily distinguished by their black-and-white coloration, large dorsal fin and a sleek, streamlined body.'
-}];
+}];*/
 
 const addCards = (items) => {
     items.forEach(item => {
         let itemToAppend = '<div class="col s4 center-align">'+
-                '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
+                '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.path+'">'+
                 '</div><div class="card-content">'+
-                '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
+                '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.subTitle+'</a></p></div>'+
                 '<div class="card-reveal">'+
                 '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
                 '<p class="card-text">'+item.description+'</p>'+
@@ -30,7 +30,7 @@ const addCards = (items) => {
         $("#card-section").append(itemToAppend)
     });
 }
-
+/*
 const formSumitted = () => {
     let formData = {};
     formData.firstName = $('#first_name').val();
@@ -48,4 +48,58 @@ $(document).ready(function(){
     });
     addCards(cardList);
     $('.modal').modal();
+});
+const addCards = (items) => {
+    items.forEach(item => {
+        let itemToAppend = '<div class="col s4 center-align">'+
+                '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.path+'">'+
+                '</div><div class="card-content">'+
+                '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
+                '<div class="card-reveal">'+
+                '<span class="card-title grey-text text-darken-4">'+item.subTitle+'<i class="material-icons right">close</i></span>'+
+                '<p class="card-text">'+item.description+'</p>'+
+                '</div></div></div>';
+        $("#card-section").append(itemToAppend)
+    });
+}*/
+
+const formSumitted = () => {
+    let formData = {};
+    formData.title = $('#title').val();
+    formData.path = $('#path').val();
+    formData.subTitle = $('#subTitle').val();
+    formData.description = $('#description').val();
+
+    console.log(formData);
+    postCat(formData);
+}
+
+function postCat(cat) {
+    $.ajax({
+        url:'/api/cat',
+        type:'POST',
+        data:cat,
+        success: (result) => {
+            if (result.statusCode === 201) {
+                alert('cat posted');
+            }
+        }
+    });
+}
+
+function getAllCats() {
+    $.get('/api/cats',(result)=>{
+        if (result.statusCode === 200) {
+            addCards(result.data);
+        }
+    });
+}
+
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
+    $('#formSubmit').click(()=>{
+        formSumitted();
+    });
+    $('.modal').modal();
+    getAllCats();
 });
